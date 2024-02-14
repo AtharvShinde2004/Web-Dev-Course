19eb30937c2bdd4598458f8354f9c394a6a8b163
Atharv Shinde <109618836+AtharvShinde2004@users.noreply.github.com>
2024-02-14 22:03:42 +0530

    Lecture 32

---
+++ Lecture 32/script.js
@@ -0,0 +1,30 @@
+// console.log("Welcome to Web Dev Course");
+
+// x = 5;
+// y = 6;
+// z = x + y;
+// console.log(z);
+
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+
+// The Var Keyword was used in all javascript code from 1995 to 2015
+// Now let and const are recommended instead of var for better practice
+
+let x = Null;
+let y = 15;
+let z = x + y;
+console.log(z);
+
+
+// Data Types in Javascript
+// Number: A numeric value represented by a double precision floating point number. It can be positive or negative, with no decimal places if it is an integer. Examples include -4783
+// String - A sequence of characters enclosed by single or double quotes
+// Boolean - true / false
+// Null - Used when a variable is empty/not assigned yet
+// Undefined - When a variable has not been declared at all
+// Object - A collection of properties/data with key-value pairs
+// Array - A collection of items stored in an ordered list
+// Function - A piece of code that can be executed whenever needed
+
